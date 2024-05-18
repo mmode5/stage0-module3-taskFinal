93127fecd48a1b524788d1b1172eba2e4fdcfcb6
maiko <maiko.mode5@gmail.com>
2024-05-17 21:14:00 -0400

    done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,6 +1,16 @@
 package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
-    public void calculateSum(int number) {
+    public  void calculateSum(int number) {
+        int sum = 0;
+        int length = Integer.toString(number).length();
+        while(length >0 ){
+            sum += number % 10;
+            number = (number - number %10 )/10;
+            length--;
+        }
+        System.out.println(sum);
     }
+
+
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,16 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
-    public void revert(int number) {
+    public  void revert(int number) {
+        int result  = 0;
+        int length = Integer.toString(number).length();
+        for(int i = length; i > 0; i--){
+           result += number %10 * Math.pow(10, i);
+           number = (number -  number %10)/10;
+        }
+        System.out.println(
+                result/10
+        );
     }
+
 }
